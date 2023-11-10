91a04d91995bdccfc9c530aaf11766f51aa2bffb
0920GX <ltla9487@gmail.com>
2023-11-10 18:48:18 +0800

    Tako
    add Transform DisplayName to Label

---
+++ label_trans.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import re
+class keyword_search():
+    def __init__(self):
+        self.df = pd.read_excel('./data_label.xlsx')
+        self.df["DisplayName"] = self.df["DisplayName"].str.lower()
+        
+        self.eng_dict = dict(zip(self.df["DisplayName"],self.df["LabelName"]))
+        self.zh_dict = dict(zip(self.df["ChineseName"],self.df["LabelName"]))
+    def search(self, keyword):
+        if re.search(u'[\u4e00-\u9fff]+',keyword):
+            try:
+                return self.zh_dict[keyword]
+            except KeyError:
+                return "關鍵字不存在!"
+        elif re.search(r'[a-z]+',keyword):
+            try:
+                return self.eng_dict[keyword]
+            except KeyError:
+                return "Keyword does not exist."
+        else:
+            return "輸入格式錯誤"
+
+
+seacher = keyword_search()
+keyword = str(input()).lower()
+result = seacher.search(keyword)
+print(result)
