7ab3605c7f630d2f14b1efd4ffc9f36af82eade8
ken6078 <52444487+ken6078@users.noreply.github.com>
2023-11-11 15:27:57 +0800

    fix: fix test fail with merge wromg

--- src/photohash.py
+++ src/photohash.py
@@ -2,7 +2,8 @@ from PIL import Image
 
 def average_hash(image: Image, hash_size=8):
     """ Compute the average hash of the given image. """
-    pixels = list(image.getdata())avg = sum(pixels) / len(pixels)
+    pixels = list(image.getdata())
+    avg = sum(pixels) / len(pixels)
     bits = "".join(map(lambda pixel: '1' if pixel > avg else '0', pixels))
     hashformat = "0{hashlength}x".format(hashlength=hash_size ** 2 // 4)
     return int(bits, 2).__format__(hashformat)
