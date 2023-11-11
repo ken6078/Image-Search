08ef9041b412fe21b435d6ef7be4eb527063d3a2
ken6078 <52444487+ken6078@users.noreply.github.com>
2023-11-11 13:38:47 +0800

    fix: test error with wrong file path

--- tests/test_photohash.py
+++ tests/test_photohash.py
@@ -2,6 +2,6 @@ from src.photohash import average_hash
 from PIL import Image
 
 def test_average_hash():
-    with open('assets\Joeman.jpg', 'rb') as f:
+    with open('assets/Joeman.jpg', 'rb') as f:
         image = Image.open(f).resize((8, 8), Image.Resampling.LANCZOS).convert('L')
         assert average_hash(image) == 'feffffffc3030323'
