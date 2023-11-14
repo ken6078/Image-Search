ae655f6d826eb95961cc062cb8879eecdf45d755
Shaber <qq9693210000@gmail.com>
2023-11-14 15:58:35 +0800

    Add Pick_image function in template
    feat: Now you can call the Pick_image function (pick_images_with_keyword & pick_images_with_image) through the template, and you can input the keyword and seed, and upload the image.

--- image_search_project/image_search_project/settings.py
+++ image_search_project/image_search_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "frontEnd.apps.FrontendConfig",
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
